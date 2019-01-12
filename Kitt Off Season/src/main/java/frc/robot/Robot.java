59520b9219d45be60d745bc9a7b6df865c58ee67
LukeOxley <lukeoxley9@gmail.com>
2019-01-12 10:49:00 -0600

    Added Sbox controller support

--- Kitt Off Season/src/main/java/frc/robot/Robot.java
+++ Kitt Off Season/src/main/java/frc/robot/Robot.java
@@ -7,35 +7,53 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
- 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+
+  public WPI_TalonSRX talonThree;
+  public WPI_TalonSRX talonFour;
+ // public Joystick j;
+  public DifferentialDrive b;
+  public Compressor compressor;
+  public XboxController j;
+  public Solenoid shifter;
   @Override
   public void robotInit() {
-   
+  
+    talonThree = new WPI_TalonSRX(0);
+    talonFour = new WPI_TalonSRX(1);
+    talonThree.set(ControlMode.PercentOutput, 0);
+    talonFour.set(ControlMode.PercentOutput, 0);
+    //talonThree.setInverted(true);
+    // sensorThree = talonThree.getSensorCollection();
+    CameraServer.getInstance().startAutomaticCapture();
+    compressor=new Compressor();
+    b=new DifferentialDrive(talonThree,talonFour);
+
+   // j = new Joystick(0);
+   j=new XboxController(0);
+   shifter = new Solenoid(0);
   }
 
-  
   @Override
   public void robotPeriodic() {
   }
 
-  
+ 
   @Override
   public void autonomousInit() {
-   
+    
   }
 
   /**
@@ -43,20 +61,62 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-   
+    
   }
 
   /**
    * This function is called periodically during operator control.
    */
+@Override
+public void teleopInit() {
+  compressor.start();
+}
+
   @Override
   public void teleopPeriodic() {
+   // b.arcadeDrive(j.getY(), j.getX());
+   driverArcadeDrive();
   }
+
+  boolean driveReduction=false;
+	double driveReductionAmount = .7; //remember a higher number means less reduction
+	
+	double turnReduction=.85;
+	
+	public void driverArcadeDrive() {
+		double speed=0;
+		double turn=j.getX(Hand.kLeft)*turnReduction;
+		if(j.getTriggerAxis(Hand.kRight)>.05) {
+			speed=j.getTriggerAxis(Hand.kRight);			
+		}else if(j.getTriggerAxis(Hand.kLeft)>.05) {
+			speed=-j.getTriggerAxis(Hand.kLeft);
+			turn=-turn;
+		}else {
+			speed=0;
+		}
+		if(driveReduction) {
+			turn=turn*driveReductionAmount;
+			speed=speed*driveReductionAmount;
+		}
+		b.arcadeDrive(speed, turn);
+	}
+
+  
+public void shifterPeriodic(){
+  if(j.getBumper(Hand.kLeft))shifter.set(true);
+  else if (j.getBumper(Hand.kRight))shifter.set(false);
+}
 
   /**
    * This function is called periodically during test mode.
    */
   @Override
   public void testPeriodic() {
+  }
+
+
+  @Override
+  public void disabledInit() {
+    compressor.stop();
   }
 }
